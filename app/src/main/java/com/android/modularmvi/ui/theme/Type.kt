eccf679dede66416c442e58a19c0c0f20a620735
NovaLogics <rajitha.shavinda@gmail.com>
2025-02-06 16:37:19 +0530

    config(res): add fonts

--- app/src/main/java/com/android/modularmvi/ui/theme/Type.kt
+++ app/src/main/java/com/android/modularmvi/ui/theme/Type.kt
@@ -2,11 +2,17 @@ package com.android.modularmvi.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.android.modularmvi.R
 
-// Set of Material typography styles to start with
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -14,7 +20,13 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
